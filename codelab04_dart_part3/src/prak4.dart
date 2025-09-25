0663860a149fdaf8ec442c899dcf144bc8572daf
geriiy <gheriyaj@gmail.com>
2025-09-25 11:58:31 +0700

    prak4_langlah3

--- codelab04_dart_part3/src/prak4.dart
+++ codelab04_dart_part3/src/prak4.dart
@@ -5,4 +5,16 @@ void main() {
   print(list); // mencetak list asli
   print(list2); // mencetak list baru dengan elemen tambahan
   print(list2.length); // mencetak panjang list2
+
+  var list1 = [1, 2, null]; // List dengan elemen termasuk null
+  print(list1);
+
+  var list3 = [0, ...?list1]; // Null-aware spread operator
+  print(list3);
+  print(list3.length);
+
+  // Tambahkan list berisi NIM menggunakan spread operator
+  var nimList = ['2', '3', '4', '1', '7', '2', '0', '1', '0', '9'];
+  var fullList = [0, ...nimList];
+  print(fullList);
 }
