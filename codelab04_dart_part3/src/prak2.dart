b7f08a59c54d3d9eb6ee297926b5a11a9e29e4d0
geriiy <gheriyaj@gmail.com>
2025-09-25 11:46:42 +0700

    prak2_langkah3

--- codelab04_dart_part3/src/prak2.dart
+++ codelab04_dart_part3/src/prak2.dart
@@ -1,4 +1,19 @@
 void main() {
-  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
-  print(halogens);
+  var names1 = <String>{};
+  Set<String> names2 = {};
+  var names3 = {};
+
+  // Tambahkan elemen ke Set
+  names1.add('Gheriya');
+  names1.add('22050123');
+
+  names2.addAll({'Gheriya', '22050123'});
+
+  // Tambahkan elemen ke Map
+  names3['nama'] = 'Gheriya';
+  names3['nim'] = '22050123';
+
+  print('names1 (Set): $names1');
+  print('names2 (Set): $names2');
+  print('names3 (Map): $names3');
 }
