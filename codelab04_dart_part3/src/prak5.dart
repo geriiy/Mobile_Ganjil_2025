8196070189490b72aaa881037dcf5d31671946fd
geriiy <gheriyaj@gmail.com>
2025-09-25 12:53:14 +0700

    prak5_langkah3

--- codelab04_dart_part3/src/prak5.dart
+++ codelab04_dart_part3/src/prak5.dart
@@ -1,4 +1,13 @@
+// Fungsi di luar main() untuk menukar nilai dalam record
+(int, int) tukar((int, int) record) {
+  var (a, b) = record; // destructuring record
+  return (b, a); // tukar posisi
+}
+
 void main() {
-  var record = ('first', a: 2, b: true, 'last');
-  print(record);
+  var pasangan = (7, 9); // record awal
+  print('Sebelum tukar: $pasangan');
+
+  var hasilTukar = tukar(pasangan); // panggil fungsi tukar
+  print('Sesudah tukar: $hasilTukar');
 }
